c3b64042f26ad0c031c2e800762789b11dec756b
karan1_2 <karankumar2004122@gmail.com>
2026-07-11 09:56:44 +0530

    write readme remove

--- frontend/write-readme.cjs
+++
@@ -1,8 +0,0 @@
-'use strict';
-const fs = require('fs');
-const path = require('path');
-const target = path.join(__dirname, 'README.md');
-const REPO = 'https://github.com/karankumar12345/groweasy-csv-importer';
-const CLONE = 'https://github.com/karankumar12345/groweasy-csv-importer.git';
-const lines = [];
-const L = (s) => lines.push(s);
